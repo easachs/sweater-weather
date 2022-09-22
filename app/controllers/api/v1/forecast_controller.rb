22701666b257065289285378b09e4ef62da7339c
Eli Sachs <easachs13@gmail.com>
2022-09-21 23:41:13 -0600

    Setup: DB create

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ForecastController < ApplicationController
+      def index
+        # params[:location]
+      end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,6 +68,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  # config.filter_sensitive_data('<api_key>') { ENV['some_api_key'] }
+  config.filter_sensitive_data('<mapquest_key>') { ENV['map_key'] }
   config.configure_rspec_metadata!
 end
