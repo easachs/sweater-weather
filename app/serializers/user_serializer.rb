bc5f5efc20c358f5cc00af724808f5edec981040
Eli Sachs <easachs13@gmail.com>
2022-09-24 02:18:22 -0600

    Feat: add user serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class UserSerializer
+  def self.new_user_response(user)
+    {
+      data: {
+        type: 'users',
+        id: user.id,
+        attributes: {
+          email: user.email,
+          api_key: user.api_key
+        }
+      }
+    }
+  end
+end
