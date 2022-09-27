31f59025be0059fd3cd5a3b873786aa2de1087b0
Eli Sachs <easachs13@gmail.com>
2022-09-27 15:22:01 -0600

    Refactor: abstract controllers, add secure token

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,11 +4,10 @@ module Api
   module V1
     class SessionsController < ApplicationController
       def create
-        user_params = JSON.parse(request.raw_post, symbolize_names: true) unless ['', nil].include?(request.raw_post)
-        user_params ? user = User.find_by(email: user_params[:email]) : nil
-        if !user_params
+        user = User.find_by(email: request_params[:email]) unless !request_params
+        if !request_params
           render json: { error: 'missing JSON payload in request body' }, status: 400
-        elsif user&.authenticate(user_params[:password])
+        elsif user&.authenticate(request_params[:password])
           render json: UserSerializer.new_user_response(user)
         else
           render json: { error: 'invalid credentials' }, status: 401
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, :password_confirmation
   has_secure_password
-
-  before_create do |user|
-    user.api_key = user.create_api_key
-  end
-
-  def create_api_key
-    loop do
-      key = SecureRandom.hex
-      break key unless User.exists?(api_key: key)
-    end
-  end
+  has_secure_token :api_key
 end
